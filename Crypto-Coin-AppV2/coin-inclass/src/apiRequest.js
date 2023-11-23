02553368e4518f774d896dd29cfaed36c3dd2818
BruceHill <brucehillwalley@gmail.com>
2023-11-23 23:55:56 +0300

    delete

--- Crypto-Coin-AppV2/coin-inclass/src/apiRequest.js
+++ Crypto-Coin-AppV2/coin-inclass/src/apiRequest.js
@@ -1,14 +1,21 @@
+import { renderCoins } from "./renderData"
 export const apiRequest=async(input)=>{
     
-    const API_KEY = "coinrankingab4c994a5f5bcaffc18ef98eb56ec6c7d0b9d41dbf5ec91d"
-    const options = { headers: { "x-access-token": API_KEY } }
-  
+    //? 
+    const options = { headers: { "x-access-token": import.meta.env.VITE_API_KEY } }
+  console.log();
    const url = `https://api.coinranking.com/v2/coins?search=${input}`
     try {
         //?status u ekle
           const res = await fetch(url,options)
           const json=await res.json()
-          console.log(json.data.coins[0]);
+          if (json.data.coins.length===0) {
+            alert("Coin can not be found")
+          }else{
+            // console.log(json.data.coins[0]);
+            renderCoins(json.data.coins[0])
+          }
+          
         
          } catch (error) {
           console.log(error);
---
+++ Crypto-Coin-AppV2/coin-inclass/src/renderData.js
@@ -0,0 +1,39 @@
+export const renderCoins = (coin) => {
+  //?havada destructer da yapabilirsin
+  const { price, name, iconUrl, change, symbol, rank } = coin;
+
+  const coinsUl = document.querySelector("main .coins");
+
+  const newCoinLi = document.createElement("li");
+
+  newCoinLi.classList.add("coin");
+  newCoinLi.innerHTML = `
+    <div class="remove-icon">
+
+    <i class="fas fa-window-close"></i>
+
+    </div>
+   <h2>
+   <span>${name}</span>
+   <sup>${symbol}</sup>
+   </h2>
+
+   <div class="coin-temp">${Number(price).toFixed(2)}</div>
+   <figure>
+        <img src="${iconUrl}" /}
+        <figcaption style="color:${change < 0 ? "red" : "green"}">
+        <i class="fa-solid fa-chart-line"></i>
+        <span>${change || "0"}%</span>
+        
+        </figcaption>
+        <div>RANK:${rank}</div>
+        </figure>
+
+`
+  coinsUl.append(newCoinLi);
+
+  //? sil butonu için event tanımmlanması
+  newCoinLi.querySelector(".remove-icon").addEventListener("click", ()=>{
+    newCoinLi.remove()
+  })
+};
